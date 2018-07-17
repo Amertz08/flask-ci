1737a5f8e4765bc292a60d1e147f024676d621ed
Adam Mertz <adammertz@gmail.com>
2018-07-17 18:37:54 -0500

    wrote simple test suite

---
+++ test_endpoints.py
@@ -0,0 +1,21 @@
+import unittest
+
+from flask import url_for
+from flask_testing import TestCase
+
+from app import create_app
+
+
+class TestApp(TestCase):
+
+    def create_app(self):
+        return create_app()
+
+    def test_index(self):
+        resp = self.client.get(url_for('main.index'))
+        self.assert200(resp)
+        self.assertEqual(resp.data, b'Hello')
+
+
+if __name__ == '__main__':
+    unittest.main()
